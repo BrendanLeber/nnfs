0af9454d6dbb6b185d88593bc41415aada8158f2
Brendan Leber <brendan@brendanleber.com>
2020-11-19 15:15:19 -0800

    Code through the end of Chapter 3.

---
+++ nnfs_code.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import nnfs
+from nnfs.datasets import spiral_data
+
+
+nnfs.init()
+
+
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons) -> None:
+        """Initialize weights and biases."""
+        self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+
+
+    def forward(self, inputs) -> None:
+        """Calculate output values from inputs, weights, and biases."""
+        self.output = np.dot(inputs, self.weights) + self.biases
+
+
+# create dataset
+X, y = spiral_data(samples=100, classes=3)
+
+# create dense layer with 2 input features and 3 output values
+dense1 = Layer_Dense(2, 3)
+
+# perform a forward pass of our training data through this layer
+dense1.forward(X)
+
+# see the output of the first few samples
+print(dense1.output[:5])
